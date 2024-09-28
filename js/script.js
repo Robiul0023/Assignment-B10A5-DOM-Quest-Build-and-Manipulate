80ccf577aea85657dde2237ddd3ddae9324bb8b1
robiul0023 <asiq007@gmail.com>
2024-09-29 02:55:50 +0600

    nav section added

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+
+ const btnDonation = document.getElementById('donation');
+ btnDonation.addEventListener('click',function(){
+  btnDonation.style.backgroundColor = "lime";
+
+ const historyBtn =  document.getElementById('history');
+ historyBtn.style.backgroundColor = 'white';
+
+ });
+
+
+ const btnHistory = document.getElementById('history');
+ btnHistory.addEventListener('click', function(){
+    btnHistory.style.backgroundColor = 'lime';
+    const donationBtn = document.getElementById('donation');
+    donationBtn.style.backgroundColor = 'white';
+    
+
+
+
+ });
+
+ 
+
+
+   
